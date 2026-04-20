431eb042b4524c33d8c371b79c19f25e65e2fced
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-20 13:56:32 +0400

    Variable excercise

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,10 @@
 let js = 'amazing';
 if (js === "amazing") alert("Javascript is fun");
 
-let result = 40 + 8 + 23 - 10;
-console.log(result);
+let firstName = "Taha"; //Default convention for variable names 
+let last_name = "Dama"; //Alternative but not preffered 
+let PI = 3.145; // Constant variable naming is all CAPS 
+let firstCompany = "B.U.T" 
+let myCurrentCompany = "S.B.E"
+
+console.log("my name is " + firstName + " and i work at " + myCurrentCompany);
