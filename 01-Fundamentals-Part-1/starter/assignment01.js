fcd0df401414fe79d629aa27ef86c9497d4a313b
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-20 16:19:42 +0400

    Create and add assignment 1 code

---
+++ 01-Fundamentals-Part-1/starter/assignment01.js
@@ -0,0 +1,5 @@
+let country = "United Arab Emirates"; 
+let contient = "Asia"
+let population = 11.5;
+
+console.log("I live in " + country + " located in " + contient + " with a population of " + population + " million");
