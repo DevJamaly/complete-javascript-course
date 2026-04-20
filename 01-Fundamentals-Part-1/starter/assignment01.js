f090adcc8c333fb22411e6c31e0c0921ea104ec1
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-20 16:48:28 +0400

    add data and value types assignment and code

--- 01-Fundamentals-Part-1/starter/assignment01.js
+++ 01-Fundamentals-Part-1/starter/assignment01.js
@@ -1,5 +1,12 @@
 let country = "United Arab Emirates"; 
 let contient = "Asia"
 let population = 11.5;
+let isIsland = false;
+let language;
 
 console.log("I live in " + country + " located in " + contient + " with a population of " + population + " million");
+
+console.log("isIsland: " + isIsland + " | " + typeof(isIsland));
+console.log("population: " + population + " | " + typeof(population));
+console.log("country: " + country + " | " + typeof(country));
+console.log("language: " + language + " | " + typeof(language));
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/* let js = 'amazing';
 if (js === "amazing") alert("Javascript is fun");
 
 let firstName = "Taha"; //Default convention for variable names 
@@ -7,4 +7,27 @@ let PI = 3.145; // Constant variable naming is all CAPS
 let firstCompany = "B.U.T" 
 let myCurrentCompany = "S.B.E"
 
-console.log("my name is " + firstName + " and i work at " + myCurrentCompany);
+console.log("my name is " + firstName + " and i work at " + myCurrentCompany); */
+
+
+// --------------DATA TYPES---------------
+let jsIsFun = true;
+
+console.log(jsIsFun);
+console.log(typeof(jsIsFun));
+
+jsIsFun = 'YES!';
+console.log(jsIsFun);
+console.log(typeof(jsIsFun));
+
+let year;
+console.log(year);
+console.log(typeof(year));
+
+year = 1991;
+console.log(year);
+console.log(typeof(year));
+
+year = null;
+console.log(year);
+console.log(typeof(year));
