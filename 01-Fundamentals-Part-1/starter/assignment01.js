366acae0d527eb1cfbc41e17ea79efcb3a65f56a
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-22 12:28:41 +0400

    Add switch statements excercise and lesson code

--- 01-Fundamentals-Part-1/starter/assignment01.js
+++ 01-Fundamentals-Part-1/starter/assignment01.js
@@ -50,3 +50,27 @@ else console.log(`No borders`); */
 //Logical Operators
 if (language === 'English' && population < 50 && !isIsland) console.log(`You should live in ${country}`);
 else console.log(`${country} does not meet your criteria`);
+
+//Switch Statements 
+switch (language.toLowerCase()) {
+    case 'chinese':        
+    case 'mandarin':
+        console.log(`MOST number of native speakers!`) ; 
+        break;
+    case 'spanish':
+        console.log(`2nd place in number of native speakers`) ;   
+        break;
+    case 'hindi':
+        console.log(`Number 4`) ;    
+        break;
+    case 'arabic':
+        console.log(`5th most spoken language`) ;   
+        break;
+    case 'english':
+        console.log(`3rd place`) ;   
+        break;
+
+    default:
+        console.log(`Great language too :D`) ;
+        break;
+}
