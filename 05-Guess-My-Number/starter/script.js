e045865fd88fee5106600f20dc6c1a7580b4e84f
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-28 16:41:33 +0400

    Add code for changing DOM elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,4 +2,18 @@
 
 //Document query selector allows us to get DOM elements
 const msgElement = document.querySelector('.message');
-console.log(msgElement);
+console.log(msgElement.textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+const secretNum = document.querySelector('.number');
+secretNum.textContent = Math.round(Math.random() * 19) + 1;
+console.log(secretNum.textContent);
+
+const score = document.querySelector('.score');
+console.log(score.textContent);
+
+const inputField = document.querySelector('.guess');
+inputField.value = 23;
+console.log(inputField.value);
