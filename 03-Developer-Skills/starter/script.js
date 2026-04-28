c59a722a9fab5518184e934c1957934db581234f
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-28 11:01:29 +0400

    Editor setup with prettier formatter

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+const y = 'Taha';
+
+if (x === 23) console.log(`value is 23`);
+const calcAge = birthYear => 2037 - birthYear;
+const calcAgeFull = (birthYear, currentYear) => currentYear - birthYear;
+console.log(`my age is ${calcAgeFull(1996, 2026)}`);
