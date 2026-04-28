70fcab51bd309cf1c1be809c5e5a0f9e95ef0d9a
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-28 17:39:58 +0400

    Add code to handle button click to get form input

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //--------------DOM QUERYING--------------------
-//Document query selector allows us to get DOM elements
+/* //Document query selector allows us to get DOM elements
 const msgElement = document.querySelector('.message');
 console.log(msgElement.textContent);
 
@@ -17,4 +17,39 @@ console.log(score.textContent);
 
 const inputField = document.querySelector('.guess');
 inputField.value = 23;
-console.log(inputField.value);
+console.log(inputField.value); */
+
+//--------------JS Event Handling--------------------
+
+const clickEventHandler = function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    msgElement.setValue('⛔ No Number!');
+  } else {
+    msgElement.setValue(guess);
+  }
+};
+
+// const message = document.querySelector('.message');
+// console.log(message.textContent);
+// function setMessageValue(value) {
+//   message.textContent = value;
+// }
+
+const msgElement = {
+  a: 1,
+  setValue: function (value) {
+    if (!this.node) {
+      this.node = document.querySelector('.message');
+    }
+    this.node.textContent = value;
+  },
+};
+
+const checkBtn = document.querySelector('.check');
+// checkBtn.addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+// });
+checkBtn.addEventListener('click', clickEventHandler);
