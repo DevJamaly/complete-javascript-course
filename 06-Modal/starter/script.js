0b223a2d285af1931d14e5f9e55489b4f499639b
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-04-29 21:57:28 +0400

    Add code for modal event listeners and CSS class manipulation

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+function showModalWindow(event) {
+  //   const button = event.currentTarget;
+  //   console.log(`Button Clicked ${button.textContent}`);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function hideModalWindow(event) {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function handleKeyPress(event) {
+  if (!event.key || event.key !== 'Escape') return;
+  if (modal.classList.contains('hidden')) return;
+  hideModalWindow();
+}
+
+//Add event listeners for the show modal buttons
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', showModalWindow);
+
+//Add event listeners for close button on modal
+btnCloseModal.addEventListener('click', hideModalWindow);
+overlay.addEventListener('click', hideModalWindow);
+
+//Add event listener for handling Esc key press in the window!
+document.addEventListener('keydown', handleKeyPress);
