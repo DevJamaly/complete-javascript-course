6307bac4464a8e746e47dd190daa5d46a07639d0
DevJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-05-06 17:45:35 +0400

    Add code for Hoisting and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// Declared in global scope
+//--------------------VARIABLE SCOPES-------------------
+/* // Declared in global scope
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   //   console.log(firstName); //Accessing global variable through scope chain
@@ -33,4 +34,48 @@ function calcAge(birthYear) {
 
 // calcAge(1996); //We get a reference error here because in linear execution we haven't come across the firstName variable yet !
 const firstName = 'Taha';
-calcAge(1996);
+calcAge(1996); */
+
+//--------------------HOISTING AND TDZ-------------------
+// Variable hoisting
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Taha';
+let job = 'Programmer';
+const year = 1996;
+
+//Functions
+
+console.log(addDecl(2, 3)); //Runs correctly since its hoisted and can run before initialization
+// console.log(addExpr(2, 3)); //Reference error ! acts as a variable which is uninitialized.
+// console.log(addArrow(2, 3)); //Shows not a function since var is hoisted but is undefined.
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//Example on the dangers of 'var'
+
+//We want to check if numProducts is 0 which is a falsey value so we delete the shopping cart
+if (!numProducts) deleteShoppingCart(); // This runs and deletes cart since undefined is a falsey value and triggers this condition
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log(`All products deleted!`);
+}
+
+var x = 1; //var shows up in the window object!
+let y = 2;
+const z = 3;
+
+console.log(`X: ${x === window.x}`);
+console.log(`Y: ${y === window.y}`);
+console.log(`Z: ${z === window.z}`);
